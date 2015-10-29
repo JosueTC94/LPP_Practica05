e8df186dfb84003e1af6d8183af1078435209fc3
josuetc94 <alu0100763492@ull.edu.es>
2015-10-29 18:59:43 +0000

    Actualizando asserts

--- test/fraccionario_spec.rb
+++ test/fraccionario_spec.rb
@@ -7,13 +7,21 @@ class Test_Fraccion < Test::Unit::TestCase
     @unidad = Numero_fraccionario.new(1,1)
   end
   def test_simple
-    assert_equal(Numero_fraccionario.new(1,2).to_s, @origen.to_s) #Invoco a los metodos
-    assert_equal(Numero_fraccionario.new(1,1).to_s, @unidad.to_s,"Holaaaaa")
-    assert_equal((Numero_fraccionario.new(5,2)).to_s, (@origen.multiplico_valor(5)).to_s, "Producto escalar")
-    assert_equal("3/2", (@origen + @unidad).to_s)
-    assert_equal("-1/2", (@origen - @unidad).to_s)
-    assert_not_equal("1/3", (@origen * @unidad).to_s)
+    assert_equal("1/2".to_s, @origen.to_s) #Invoco a los metodos
+
+    assert_equal("1/1".to_s, @unidad.to_s)
+  
+    assert_equal("5/2", (@origen.multiplico_valor(5)).to_s, "Producto escalar")
+
     assert_equal("1/2",(@origen * @unidad).to_s)
+    
     assert_equal("1/2", (@origen / @unidad).to_s)
+
+    assert_equal("3/2", (@origen + @unidad).to_s)
+
+    assert_equal("-1/2", (@origen - @unidad).to_s)
+
+    assert_not_equal("1/3", (@origen * @unidad).to_s)
+  
   end
 end
